57626131b3db704b04e57bbc9e4913ef09402481
Your Name <tranquanghuy1280@gmail.com>
2019-10-21 16:49:16 +0700

    home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.turbolinks
+//= require bootstrap
 //= require_tree
+
---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#itemslider').carousel({ interval: 4000 });
+    $('#itemslider1').carousel({ interval: 4000 });
+    $('.carousel-showmanymoveone .item').each(function(){
+        var itemToClone = $(this);
+
+        for (var i=1;i<4;i++) {
+            itemToClone = itemToClone.next();
+
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            itemToClone.children(':first-child').clone()
+            .addClass("cloneditem-"+(i))
+            .appendTo($(this));
+        }
+    });
+});
+
+
